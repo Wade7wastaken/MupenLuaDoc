adddc1f94431d36099372acb14076ee6f827430e
David C <david@thecallenders.com>
2023-06-04 22:24:48 -0500

    fix javascript

--- script/index.js
+++ script/index.js
@@ -1,15 +1,13 @@
-(() => {
-	const coll = document.getElementsByClassName("collapsible");
+const coll = document.getElementsByClassName("collapsible");
 
-	for (let i = 0; i < coll.length; i++) {
-		coll[i].addEventListener("click", () => {
-			this.classList.toggle("active");
-			const content = this.nextElementSibling;
-			if (content.style.maxHeight) {
-				content.style.maxHeight = null;
-			} else {
-				content.style.maxHeight = content.scrollHeight + "px";
-			}
-		});
-	}
-})();
+for (let i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		const content = this.nextElementSibling;
+		if (content.style.maxHeight) {
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	});
+}
