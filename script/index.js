03acce5ce3ebba007811835165550bcf3f488ab0
tjk113 <tjkuhl1@live.com>
2023-06-12 22:46:07 -0400

    various changes: - added brief highlighting of function docs when accessed from the sidebar links - clicking anywhere on the function buttons in the sidebar now works - removed built-in modules from requirements.txt

--- script/index.js
+++ script/index.js
@@ -11,3 +11,15 @@ for (let i = 0; i < coll.length; i++) {
 		}
 	});
 }
+
+async function highlightFunc(anchorId) {
+	/* Add 'highlight' class to element, which
+	causes it to briefly become highlighted */
+	let curFunc = document.getElementsByName(anchorId)[0];
+	curFunc.classList.add('highlight');
+	// Wait for fade to end
+	await new Promise(r => setTimeout(r, 1510));
+	/* Remove 'highlight' class from element, so that it can be
+	added back again if the function link is clicked again */ 
+	curFunc.classList.remove('highlight');
+}
